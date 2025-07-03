df369e9047316c0dddec0b30ceedb538daa05f0b
Amin Malekloo <amin.malekloo@persgroep.net>
2025-07-03 17:49:43 +0200

    improve latex output format

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -15,12 +15,12 @@ object Main extends App {
     }
   }
 
-  parse("2^30(x)^2 / sin(x)*3^4") { expression =>
+  parse("2^30(x)^2 / 3^4sin(x) + 2a") { expression =>
     println(s"expression is ${expression.simplified.show}")
-    val derivative = expression.derivativeOver(x)
-    println(s"derivative is ${derivative.show}")
-    println(s"derivative is ${Simplifier.simplify(derivative).show}")
-    println(s"derivative at point x = 1 is ${derivative.solveFor("x" -> 1)}")
+//    val derivative = expression.derivativeOver(x)
+//    println(s"derivative is ${derivative.show}")
+//    println(s"derivative is ${Simplifier.simplify(derivative).show}")
+//    println(s"derivative at point x = 1 is ${derivative.solveFor("x" -> 1)}")
     println(LatexOutput.output(expression.simplified))
   }
 }
--- src/main/scala/out/LatexOutput.scala
+++ src/main/scala/out/LatexOutput.scala
@@ -3,14 +3,41 @@ package out
 import expr.Expr
 
 object LatexOutput extends ExpressionOutput[Id, String] {
+
+  private object NotAddOrSub {
+    def unapply(e: Expr): Option[Expr] = e match {
+      case Expr.Add(_, _) | Expr.Sub(_, _) => None
+      case _                               => Some(e)
+    }
+  }
+
+  private object Function {
+    def unapply(e: Expr): Option[Expr] = e match {
+      case Expr.Sin(_) | Expr.Cos(_) | Expr.Tan(_) | Expr.Cot(_) | Expr.Func(_, _) => Some(e)
+      case _                                                                       => None
+    }
+  }
+  
+  private object VarComesNext {
+    def unapply(e: Expr): Option[Expr] = e match {
+      case Expr.Var(_) | Expr.Pow(_, Expr.Var(_)) | Expr.Pow(Expr.Var(_), _) => Some(e)
+      case _                                                                 => None
+    }
+  }
+
   private def toLatexString(e: Expr): String = e match {
     case Expr.Var(name) => name
-    case Expr.Const(num) => num.toString
+    case Expr.Const(num) => s"{${num}}"
     case Expr.e => "e"
     case Expr.Add(lhs, rhs) => s"(${toLatexString(lhs)} + ${toLatexString(rhs)})"
     case Expr.Sub(lhs, rhs) => s"(${toLatexString(lhs)} - ${toLatexString(rhs)})"
+    case Expr.Mul(lhs, Function(rhs)) => s"${toLatexString(lhs)}${toLatexString(rhs)}"
+    case Expr.Mul(Function(lhs), rhs) => s"${toLatexString(rhs)}${toLatexString(lhs)}"
+    case Expr.Mul(lhs, VarComesNext(rhs)) => s"${toLatexString(lhs)}${toLatexString(rhs)}"
+    case Expr.Mul(VarComesNext(lhs), rhs) => s"${toLatexString(rhs)}${toLatexString(lhs)}"
+    case Expr.Mul(NotAddOrSub(lhs), NotAddOrSub(rhs)) => s"${toLatexString(lhs)} * ${toLatexString(rhs)}"
     case Expr.Mul(lhs, rhs) => s"(${toLatexString(lhs)} * ${toLatexString(rhs)})"
-    case Expr.Pow(base, exponent) => s"(${toLatexString(base)} ^ ${toLatexString(exponent)})"
+    case Expr.Pow(base, exponent) => s"${toLatexString(base)} ^ ${toLatexString(exponent)}"
     case Expr.Div(dividend, divisor) => s"\\frac{${toLatexString(dividend)}}{${toLatexString(divisor)}}"
     case Expr.Neg(e) => s"-${toLatexString(e)}"
     case Expr.Sin(arg) => s"sin(${toLatexString(arg)})"
